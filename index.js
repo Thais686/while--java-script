847f1c0e2dcd6151c67e9de0de81fc95b3511971
Thaís <thaiscastro95@yahoo.com>
2024-09-23 15:32:34 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let numero
+let variavel=0
+
+numero= Number (prompt("Digitar o numero que voce quer na tabuada"))
+
+
+while(variavel>=0 && variavel<=10){
+
+  var  multiplicacao= numero*variavel
+
+  console.log(numero + "X" + variavel + "=" + multiplicacao)
+
+variavel++
+
+}
+
---
+++ index2.js
@@ -0,0 +1,20 @@
+let variavel= 1
+let num=1
+
+while(num>=1 && num<=9){
+
+while(variavel>=1 && variavel<=9){
+
+
+        var multiplicacao = variavel*num
+
+    
+        console.log(multiplicacao)
+    
+        variavel++
+}
+num++
+variavel=1
+
+}
+
